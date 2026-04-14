4727315cac09ccdd3bba8b0135af96166f3fe151
omurkanovd22 <omurkanovd22@gmail.com>
2026-04-14 19:32:55 +0600

    Initial commit

---
+++ src/test/java/kg/megalab/comp_service/CompServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.comp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
